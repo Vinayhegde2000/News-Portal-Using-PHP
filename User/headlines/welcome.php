c9e963af6592d7c7176dc3bdc020176561d93890
Vinayhegde2000 <56596753+Vinayhegde2000@users.noreply.github.com>
2021-01-21 04:35:09 +0530

    Add files via upload

---
+++ User/headlines/welcome.php
@@ -0,0 +1,154 @@
+﻿
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+     <link rel="stylesheet" type="text/css"href="css/style.css">
+<link rel="stylesheet" href="style1.css">
+<link rel="stylesheet" href="search.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+ <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+    <style type="text/css">
+
+        body{ font: 14px ,sans-serif;
+margin:25px;
+margin-top:30px; }
+
+    </style>
+</head>
+<body background="back.jpg"  >
+ <input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+      <header>Menu</header>
+      <a href="blog.html" class="active">
+        <i class="fas fa-qrcode"></i>
+        <span>Blog</span>
+      </a>
+      <a href="trends.html">
+        <i class="fas fa-link"></i>
+        <span>Trending</span>
+      </a>
+      <a href="event.html">
+         <i class="fas fa-calendar"></i>
+        <span>Events</span>
+      </a>
+      <a href="about.html">
+        <i class="far fa-question-circle"></i>
+        <span>About</span>
+      </a>
+      <a href="services.html">
+        <i class="fas fa-sliders-h"></i>
+        <span>Services</span>
+      </a>
+      <a href="survey.html">
+        <i class="far fa-envelope"></i>
+        <span>Contact</span>
+
+      </a>
+
+    </div>
+
+<h3>
+</br></br>
+
+    <font face="Lato" size="6"style="color:#6223cf" >NEWS PORTAL</font>
+   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+       &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  
+ <font face="cinzel">  
+    
+    <a href='politics.html'>Politics</a>  &nbsp;  &nbsp;
+    <a href='sports.html'>Sports</a>  &nbsp;  &nbsp;
+   <a href='tech.html'>Technology</a>  &nbsp;  
+    <a href='International.html'>International</a>  &nbsp; 
+    <a href='local.html'>Local</a>  
+</h3>
+
+    <div class="page-header">
+
+        <h3 align=center style="color:#07010f">Hi</b>. Welcome to our site.</h3>
+
+    </div>
+
+    <p>
+    <a href="speech.html" target="_blank">
+      <i class="fa fa-microphone" aria-hidden="true"></i>
+        <span>Speech</span>
+      </a>
+
+
+ 
+  <h1 style="color:red"> Headlines</h1></br></br>
+<div style="border:2px solid #45443f;" >
+<font size="4px">The Gujarat government has decided to build a museum at the Statue of Unity campus site in Gujarat's Kevadiya colony to 
+showcase recollection of history that made the dream of united India possible, sources said.
+The decision was taken in a meeting chaired by Gujarat Chief Minister Vijay Rupani.
+</a></font></p>
+
+</div>
+</br><div style="border:2px solid #45443f;" >
+<p><font size="4px">India will deploy its vast election machinery to deliver 600 million doses of coronavirus vaccines to the most vulnerable people in the next 6-8 months through conventional cold chain systems, the expert leading the initiative said on Friday.
+
+
+</font></p>
+</div>
+
+</br><div style="border:2px solid #45443f;" >
+<font size="4px">India's fuel demand fell 5.0 per cent in November compared with the same month last year.
+Consumption of fuel, a proxy for oil demand, totalled 17.83 million tonnes, data from the Petroleum Planning and Analysis Cell (PPAC) of the oil ministry showed.
+</a></font></p>
+</div>
+
+</br><div style="border:2px solid #45443f;" >
+<font size="4px"> In the wake of a strike called by the transport employees demanding they be considered government employees, state-run bus services across the state were crippled on Sunday, the third day of the strike by the transport employees.
+</a></font></p>
+</div>
+
+</br><div style="border:2px solid #45443f;" >
+<font size="4px"> 60 crore COVID-19 vaccine doses, 100 people per session: This is hoe India is planning vaccination drive.
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px"> Violence broke out at Wistron Corp’s manufacturing plant, which makes iPhones, at the Narasapura industrial estate in Kolar district in the early hours of Saturday as workers ransacked the premises demanding payment of salaries on time along with overtime allowance
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px">Deadlock over farm laws likely to end in 24-48 hours, breakthrough expected in final talks: Dushyant Chautala.
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px">China wants India to accept the new normal on LAC, move on to restore bilateral ties.
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px"> Farmer unions positioned at borders in the national capital have threatened to block railway tracks across the country if the reforms are not taken down.
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px">Solar Eclipse 2020: After a dazzling meteor shower, skygazers to see another astronomical event
+</a></font></p>
+</div>
+</br><div style="border:2px solid #45443f;" >
+<font size="4px">External affairs minister S Jaishankar on Sunday called for greater integration between the foreign and military policies to more effectively deal with national security challenges and protect India’s interests around the world.
+</a></font></p>
+</div></br>
+</br></br>
+
+
+</div>
+   <footer>
+	<h2>All right reserved</h2>
+</footer>
+
+</body>
+</html>
